7fdd98fb25601a2784d37aef4d8aa382ee108fe1
jrumjantsev <jrumjantsev@travix.com>
2019-12-27 13:53:19 +0100

    week 3 - k nearest neighbours

---
+++ week3/lab_k_nearest_neighbours.py
@@ -0,0 +1,51 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+df = pd.read_csv("teleCust1000t.csv")
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
+y = df['custcat'].values
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+k = 6
+neigh = KNeighborsClassifier(n_neighbors=k).fit(X_train, y_train)
+yhat = neigh.predict(X_test)
+
+# Accuracy evaluation
+print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
+print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+Ks = 10
+mean_acc = np.zeros((Ks - 1))
+std_acc = np.zeros((Ks - 1))
+ConfustionMx = []
+for n in range(1, Ks):
+    # Train Model and Predict
+    neigh = KNeighborsClassifier(n_neighbors=n).fit(X_train, y_train)
+    yhat = neigh.predict(X_test)
+    mean_acc[n - 1] = metrics.accuracy_score(y_test, yhat)
+
+    std_acc[n - 1] = np.std(yhat == y_test) / np.sqrt(yhat.shape[0])
+
+plt.plot(range(1,Ks), mean_acc, 'g')
+plt.fill_between(range(1,Ks), mean_acc - 1 * std_acc, mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show()
+
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1)
